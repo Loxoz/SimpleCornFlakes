21138813fe3997756dfddd1af4b097c6a2610acb
Loxoz <Loxoz.yt@gmail.com>
2019-06-21 21:33:58 +0200

    Chars update

--- SimpleCornFlakes.js
+++ SimpleCornFlakes.js
@@ -1,6 +1,6 @@
-var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.split('');
+var preChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.split('');
 
-function genkey(keysize = 16) {
+function genkey(keysize = 16, chars = preChars) {
     var key = '';
     for (i_ = 0; i_ < keysize; i_ ++) {
         key += chars[Math.floor(Math.random() * chars.length)];
@@ -8,11 +8,11 @@ function genkey(keysize = 16) {
     return key;
 }
 
-var scf = (existing_arr = [], keysize = 16) => {
+var scf = (existing_arr = [], keysize = 16, chars = preChars) => {
     var cornflake = genkey(keysize);
     if (existing_arr != undefined) {
         while (existing_arr.hasOwnProperty(cornflake)) {
-            cornflake = genkey(keysize);
+            cornflake = genkey(keysize, chars);
         }
     }
     return cornflake;
