7c2c9c53ec8eea2d074b995c5d56c2f33d100350
Loxoz <Loxoz.yt@gmail.com>
2019-07-21 11:51:33 +0200

    Tests fixed

--- test/test.js
+++ test/test.js
@@ -3,29 +3,29 @@ const containOnly = require('./containOnly');
 
 describe('SimpleCornFlakes', () => {
     it('approve test util function: containOnly', () => {
-        expect( containOnly() && containOnly(["A", "B", "C"], ["A", "B", "C"]) && containOnly("ABC", "ABC") && containOnly("ABC", "ABCD") && !containOnly("ABCD", "ABC") );
+        expect( containOnly() && containOnly(["A", "B", "C"], ["A", "B", "C"]) && containOnly("ABC", "ABC") && containOnly("ABC", "ABCD") && !containOnly("ABCD", "ABC") ).toBeTruthy();
     });
     it('should be a function', () => {
-        expect(CornFlake instanceof Function);
+        expect(CornFlake).toBeInstanceOf(Function);
     });
     it('should return a random String with a length of 16', () => {
-        expect(CornFlake().length == 16);
+        expect(CornFlake().length).toBe(16);
     });
     it('should return a random String with a length of 16 (with paramter provided)', () => {
-        expect(CornFlake([], 16).length == 16);
+        expect(CornFlake([], 16).length).toBe(16);
     });
     it('should return a random String with a length of 4', () => {
-        expect(CornFlake([], 4).length == 4);
+        expect(CornFlake([], 4).length).toBe(4);
     });
     it('should return a random String with a length of 4 (with special chars provided)', () => {
-        expect(CornFlake([], 4, ["A", "B", "C", "D"]).length == 4);
+        expect(CornFlake([], 4, ["A", "B", "C", "D"]).length).toBe(4);
     });
     it('should return a random String with only A, B, C and D in int and a length of 4', function() {
         var cf = CornFlake([], 4, "ABCD");
-        expect(containOnly(cf, "ABCD") && cf.length == 4);
+        expect(containOnly(cf, "ABCD") && cf.length == 4).toBeTruthy();
     });
     it('should return a random String with the same result as first test', () => {
         var cf = CornFlake([], 4, CornFlake.defaultChars);
-        expect(containOnly(cf, CornFlake.defaultChars) && cf.length == 4);
+        expect(containOnly(cf, CornFlake.defaultChars) && cf.length == 4).toBeTruthy();
     });
 });
